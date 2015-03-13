65c56542f5bbd1302203c830b0aa1bd7ad074534
Kyle <kgiardch@gmail.com>
2015-03-13 10:22:09 -0700

    Add test for single character word/single character phrase, no match.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -20,6 +20,22 @@
 
         }
 
+        function testRepeatCounterOneNoMatch()
+        {
+          //Arrange
+          $test_repeat_counter_one_no_match = new RepeatCounter;
+          $input_word = 't';
+          $input_phrase = 'x';
+
+          //Act
+          $result = $test_repeat_counter_one_no_match->countRepeats($input_word, $input_phrase);
+
+          //Assert
+          $total_word_count = array(0);
+          $this->assertEquals($total_word_count, $result);
+
+        }
+
     }
 
 ?>
