1198ec6c7c01b2f56c88d5c2d6ea4bc64e30ab9b
Kyle <kgiardch@gmail.com>
2015-03-13 10:05:59 -0700

    Create skeleton for RepeatCounterTest.php file for spec tests.

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,5 +1,19 @@
 <?php
 
+    require_once "src/RepeatCounter.php";
 
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+        function testRepeatCounterOneCharacter
+        {
+          //Arrange
+
+          //Act
+
+          //Assert
+          
+        }
+
+    }
 
 ?>
