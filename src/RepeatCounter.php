7e6ae9818a245dd919179309206583c6f81dd7e2
Kyle <kgiardch@gmail.com>
2015-03-13 11:55:55 -0700

    Fix spacing and tabbing in RepeatCounterTest.php

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,24 +2,24 @@
 
     class RepeatCounter
     {
-      function countRepeats($input_word, $input_phrase)
-      {
-        $input_word_lower = strtolower($input_word);
-        $input_phrase_lower = strtolower($input_phrase);
+        function countRepeats($input_word, $input_phrase)
+        {
+            $input_word_lower = strtolower($input_word);
+            $input_phrase_lower = strtolower($input_phrase);
 
-        $total_word_count = array();
-        $word_in_phrase = array();
+            $total_word_count = array();
+            $word_in_phrase = array();
 
-        $word_in_phrase = explode(" ", $input_phrase_lower);
+            $word_in_phrase = explode(" ", $input_phrase_lower);
 
-        foreach($word_in_phrase as $word) {
-            if ($input_word_lower == $word) {
-                array_push($total_word_count, 1);
+            foreach($word_in_phrase as $word) {
+                if ($input_word_lower == $word) {
+                    array_push($total_word_count, 1);
+                }
             }
-        }
 
-        $final_total_count = array_sum($total_word_count);
-        return $final_total_count;
-      }
+            $final_total_count = array_sum($total_word_count);
+            return $final_total_count;
+        }
     }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -6,94 +6,86 @@
     {
         function testRepeatCounterOneCharacter()
         {
-          //Arrange
-          $test_repeat_counter_one_character = new RepeatCounter;
-          $input_word = 'a';
-          $input_phrase = 'a';
+            //Arrange
+            $test_repeat_counter_one_character = new RepeatCounter;
+            $input_word = 'a';
+            $input_phrase = 'a';
 
-          //Act
-          $result = $test_repeat_counter_one_character->countRepeats($input_word, $input_phrase);
+            //Act
+            $result = $test_repeat_counter_one_character->countRepeats($input_word, $input_phrase);
 
-          //Assert
-          $this->assertEquals(1, $result);
-
+            //Assert
+            $this->assertEquals(1, $result);
         }
 
         function testRepeatCounterOneNoMatch()
         {
-          //Arrange
-          $test_repeat_counter_one_no_match = new RepeatCounter;
-          $input_word = 't';
-          $input_phrase = 'x';
+            //Arrange
+            $test_repeat_counter_one_no_match = new RepeatCounter;
+            $input_word = 't';
+            $input_phrase = 'x';
 
-          //Act
-          $result = $test_repeat_counter_one_no_match->countRepeats($input_word, $input_phrase);
+            //Act
+            $result = $test_repeat_counter_one_no_match->countRepeats($input_word, $input_phrase);
 
-          //Assert
-          $this->assertEquals(0, $result);
-
+            //Assert
+            $this->assertEquals(0, $result);
         }
 
         function testRepeatCounterTwoCharacter()
         {
-          //Arrange
-          $test_repeat_counter_two_character = new RepeatCounter;
-          $input_word = 'so';
-          $input_phrase = 'so';
+            //Arrange
+            $test_repeat_counter_two_character = new RepeatCounter;
+            $input_word = 'so';
+            $input_phrase = 'so';
 
-          //Act
-          $result = $test_repeat_counter_two_character->countRepeats($input_word, $input_phrase);
+            //Act
+            $result = $test_repeat_counter_two_character->countRepeats($input_word, $input_phrase);
 
-          //Assert
-          $this->assertEquals(1, $result);
-
+            //Assert
+            $this->assertEquals(1, $result);
         }
 
         function testRepeatCounterMultipleWords()
         {
-          //Arrange
-          $test_repeat_counter_multiple_words = new RepeatCounter;
-          $input_word = 'okay';
-          $input_phrase = 'be okay';
+            //Arrange
+            $test_repeat_counter_multiple_words = new RepeatCounter;
+            $input_word = 'okay';
+            $input_phrase = 'be okay';
 
-          //Act
-          $result = $test_repeat_counter_multiple_words->countRepeats($input_word, $input_phrase);
+            //Act
+            $result = $test_repeat_counter_multiple_words->countRepeats($input_word, $input_phrase);
 
-          //Assert
-          $this->assertEquals(1, $result);
-
+            //Assert
+            $this->assertEquals(1, $result);
         }
 
         function testRepeatCounterMultipleWordMatch()
         {
-          //Arrange
-          $test_repeat_counter_multiple_word_match = new RepeatCounter;
-          $input_word = 'at';
-          $input_phrase = 'when i was at the store at eight last night';
+            //Arrange
+            $test_repeat_counter_multiple_word_match = new RepeatCounter;
+            $input_word = 'at';
+            $input_phrase = 'when i was at the store at eight last night';
 
-          //Act
-          $result = $test_repeat_counter_multiple_word_match->countRepeats($input_word, $input_phrase);
+            //Act
+            $result = $test_repeat_counter_multiple_word_match->countRepeats($input_word, $input_phrase);
 
-          //Assert
-          $this->assertEquals(2, $result);
-
+            //Assert
+            $this->assertEquals(2, $result);
         }
 
         function testRepeatCounterCaps()
         {
-          //Arrange
-          $test_repeat_counter_caps = new RepeatCounter;
-          $input_word = 'CRAZY';
-          $input_phrase = 'Hopefully this assessment will not make me crazy';
+            //Arrange
+            $test_repeat_counter_caps = new RepeatCounter;
+            $input_word = 'CRAZY';
+            $input_phrase = 'Hopefully this assessment will not make me crazy';
 
-          //Act
-          $result = $test_repeat_counter_caps->countRepeats($input_word, $input_phrase);
+            //Act
+            $result = $test_repeat_counter_caps->countRepeats($input_word, $input_phrase);
 
-          //Assert
-          $this->assertEquals(1, $result);
-
+            //Assert
+            $this->assertEquals(1, $result);
         }
-
     }
-
 ?>
