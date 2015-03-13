0dca28c3beee9d53d8d6b9f6c9d55a8c7fa7268f
Kyle <kgiardch@gmail.com>
2015-03-13 11:01:08 -0700

    Add functionality to pass test input word CAPS and input phrase no-CAPS

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,14 +4,17 @@
     {
       function countRepeats($input_word, $input_phrase)
       {
+        $input_word_lower = strtolower($input_word);
+        $input_phrase_lower = strtolower($input_phrase);
+
         $total_word_count = array();
         $word_in_phrase = array();
 
-        $word_in_phrase = explode(" ", $input_phrase);
+        $word_in_phrase = explode(" ", $input_phrase_lower);
 
         foreach($word_in_phrase as $word)
         {
-            if($input_word == $word)
+            if($input_word_lower == $word)
             {
               array_push($total_word_count, 1);
 
